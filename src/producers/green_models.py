1cecc82e1c23db0c9498ec8e7e9ad13e53e1cf55
Denis K <denis911@users.noreply.github.com>
2026-03-18 17:34:03 +0000

    models and producer for green NY taxi dataset

---
+++ src/producers/green_models.py
@@ -0,0 +1,54 @@
+import json
+import dataclasses
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Ride:
+    lpep_pickup_datetime: int  # epoch milliseconds
+    lpep_dropoff_datetime: int  # epoch milliseconds
+    PULocationID: int
+    DOLocationID: int
+    passenger_count: int
+    trip_distance: float
+    tip_amount: float
+    total_amount: float
+
+
+# Read the parquet file and keep only these columns:
+# lpep_pickup_datetime
+# lpep_dropoff_datetime
+# PULocationID
+# DOLocationID
+# passenger_count
+# trip_distance
+# tip_amount
+# total_amount
+
+
+def ride_from_row(row):
+    return Ride(
+        lpep_pickup_datetime=int(row['lpep_pickup_datetime'].timestamp() * 1000),
+        lpep_dropoff_datetime=int(row['lpep_dropoff_datetime'].timestamp() * 1000),
+        PULocationID=int(row['PULocationID']),
+        DOLocationID=int(row['DOLocationID']),
+        passenger_count=int(row['passenger_count']),
+        trip_distance=float(row['trip_distance']),
+        tip_amount=float(row['tip_amount']),
+        total_amount=float(row['total_amount']),
+    )
+
+
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    ride_json = json.dumps(ride_dict).encode('utf-8')
+    return ride_json
+
+
+def ride_deserializer(data):
+    json_str = data.decode('utf-8')
+    ride_dict = json.loads(json_str)
+    return Ride(**ride_dict)
+
+    
---
+++ src/producers/green_producer.py
@@ -0,0 +1,55 @@
+import dataclasses
+import json
+import sys
+import time
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import pandas as pd
+from kafka import KafkaProducer
+from green_models import Ride, ride_from_row # we need green_models because our cols are different
+
+# Download NYC GREEN taxi trip data (ALL rows)
+url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-10.parquet"
+
+columns = [
+            'lpep_pickup_datetime',
+            'lpep_dropoff_datetime',
+            'PULocationID',
+            'DOLocationID',
+            'passenger_count',
+            'trip_distance',
+            'tip_amount',
+            'total_amount'
+        ]
+
+
+df = pd.read_parquet(url, columns=columns)
+
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    json_str = json.dumps(ride_dict)
+    return json_str.encode('utf-8')
+
+server = 'localhost:9092'
+
+producer = KafkaProducer(
+    bootstrap_servers=[server],
+    value_serializer=ride_serializer
+)
+t0 = time.time()
+
+topic_name = 'rides'
+
+for _, row in df.iterrows():
+    ride = ride_from_row(row)
+    producer.send(topic_name, value=ride)
+    # print(f"Sent: {ride}") # no need to print all - output is noisy...
+    time.sleep(0.01)
+
+producer.flush()
+
+t1 = time.time()
+print(f'took {(t1 - t0):.2f} seconds')
+
